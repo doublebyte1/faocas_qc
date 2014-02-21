1c463e73d6327a1317f6e6c02bf68d49aa340251
Jo <info@doublebyte.net>
2014-02-21 09:37:13 +0000

    - set app icon

--- app_solution/app_new/main.cpp
+++ app_solution/app_new/main.cpp
@@ -96,8 +96,8 @@ int main(int argc, char *argv[])
     a.setApplicationName(QObject::tr("FAOFish CAS"));
     a.setOrganizationName(QObject::tr("FAO-FIRF"));
     a.setOrganizationDomain(QObject::tr("http://www.fao.org/"));
-    //a.setApplicationVersion(QObject::tr("1.3"));
     a.setApplicationVersion(QObject::tr("-DevBuild_") + QDateTime::currentDateTime().toString());
+    a.setWindowIcon(QIcon(":app_new/medfisis.ico"));
 
     Login l;
     l.show();

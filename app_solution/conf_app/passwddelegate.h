f966dad87ed5c2358400012d48bd12d8491fb9f4
Jo <info@doublebyte.net>
2014-03-04 16:15:41 +0000

    - Added useful coments on the PasswdDelegate class

--- app_solution/conf_app/passwddelegate.h
+++ app_solution/conf_app/passwddelegate.h
@@ -8,8 +8,10 @@
 
 //! Password Delegate Class
 /*!
-This class implements a delegate between model and mapper, preventing the controls from going empty;
-because of the comboboxes, it has to derived from QSqlRelationalDelegate
+This class implements a delegate between model and mapper, allowing to encrypt and decrypt passwords.
+The passwords are stored in the database as 'cyphertext' and they are manipulated by the user as 'plain text'.
+To encrypt/decrypt the passwords a compiled key is applied, based on the SimpleCrypt class.
+To prevent the controls from going "empty", it has derived derived from NullRelationalDelegate.
 */
 
 class PasswdDelegate : public NullRelationalDelegate
@@ -25,6 +27,7 @@ class PasswdDelegate : public NullRelationalDelegate
                Delegate for all situations.
               \param colsOthers a STL list with columns that use the base class delegate
               \param colsText a STL list with columns that use special null-handling text delegate
+              \param colsPass a STL list with columns that store passwords, requiring encryption/decryption.
             */
             PasswdDelegate (QList<int> colsOthers, QList<int> colsText,QList<int> colsPass,
                 QObject *parent = 0);
